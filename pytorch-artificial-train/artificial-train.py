8045e53149292f2d73d2667687a9b30d3d375219
FocueAI <17051325951@163.com>
2023-04-23 18:06:05 +0800

    update

--- pytorch-artificial-train/artificial-train.py
+++ pytorch-artificial-train/artificial-train.py
@@ -1,10 +1,13 @@
 import hydra
 from omegaconf import DictConfig
+from trainer import Trainer
 
 
 @hydra.main(config_path="./conf", config_name="config")
 def main(cfg: DictConfig):
-    print(f'cfg:{cfg}')
+    print(f'--main--cfg:{cfg}')
+    trainer = Trainer(cfg)
+
 
 
 if __name__ == '__main__':
